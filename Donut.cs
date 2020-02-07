a57b08eeff22a417175516c12d63f15532e0c12c
JD Gault <jdgault34@gmail.com>
2020-02-07 10:15:13 -0800

    Add project files.

---
+++ Donut.cs
@@ -0,0 +1,13 @@
+﻿namespace cs_donut_shop
+{
+    class Donut
+    {
+        public string Topping { get; set; }
+        public string Flavor { get; set; }
+        public Donut(string topping, string flavor)
+        {
+            Topping = topping;
+            Flavor = flavor;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace cs_donut_shop
+{
+    class Program
+    {
+        static Dictionary<string, List<Donut>> loadDonuts()
+        {
+            string donuts = File.ReadAllText(@"./donuts.json");
+            return JsonConvert.DeserializeObject<Dictionary<string, List<Donut>>>(donuts);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
